00916f81bdb7ba6003d16312e046219b73ca07c2
Nando Dela Cruz <nando.delacruz@underwriteme.co.uk>
2018-08-20 09:12:10 +1000

    Added advance examples

--- src/index.js
+++ src/index.js
@@ -2,22 +2,41 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import rootReducer from './basic/reducers';
+import {createStore, applyMiddleware} from 'redux';
+
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+
+import rootReducer from './advanced/reducers'
+import {selectSubreddit, fetchPostsIfNeeded} from './advanced/actions'
+
+/* basicReducer
+import rootReducer from './basic/reducers';*/
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import App from './basic/components/App';
+import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer);
+/* Basic store
+const store = createStore(rootReducer);*/
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    ));
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => console.log(store.getState()));
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
 
