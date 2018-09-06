5e008472e114595b795b7b6f769d0437cc151b15
Nando Dela Cruz <nando.delacruz@underwriteme.co.uk>
2018-09-07 09:01:52 +1000

    Refactor

--- src/component/Dashboard/Client/Form.js
+++ src/component/Dashboard/Client/Form.js
@@ -48,62 +48,62 @@ export default class ClientForm extends React.Component {
         return null;
     }
 
-    render() {
+    renderForm(formApi) {
         const validateClientId = (value) => this.validateRequired('Please enter a valid Client ID.', value);
         const validateClientSecret = (value) => this.validateRequired('Please enter a valid Client Secret.', value);
 
         return (
-            <Form getApi={this.setApi} onChange={this.onChange}>
-                {formApi => (
-                    <form onSubmit={formApi.submitForm}>
-                        <TextField name="clientId" label="Client ID"
-                                   helpText="Please specify a unique identifier for your client"
-                                   placeholder="A unique identifier for the client"
-                                   formApi={formApi} validate={validateClientId} />
+            <form onSubmit={formApi.submitForm}>
+                <TextField name="clientId" label="Client ID"
+                           helpText="Please specify a unique identifier for your client"
+                           placeholder="A unique identifier for the client"
+                           formApi={formApi} validate={validateClientId} />
 
-                        <TextField name="secret" label="Client Secret"
-                                   helpText="Please specify the secret key for the client"
-                                   placeholder="A secret key used with the Client ID for authentication"
-                                   formApi={formApi} validate={validateClientSecret} />
+                <TextField name="secret" label="Client Secret"
+                           helpText="Please specify the secret key for the client"
+                           placeholder="A secret key used with the Client ID for authentication"
+                           formApi={formApi} validate={validateClientSecret} />
 
-                        <SelectField name="resourceIds" label="Resources"
-                                     helpText="Please select all resources to allow access to"
-                                     options={[
-                                         {label: 'Authentication', value: 'AUTH'},
-                                         {label: 'Finance', value: 'FINANCE'}
-                                     ]} multiple={true} />
+                <SelectField name="resourceIds" label="Resources"
+                             helpText="Please select all resources to allow access to"
+                             options={[
+                                 {label: 'Authentication', value: 'AUTH'},
+                                 {label: 'Finance', value: 'FINANCE'}
+                             ]} multiple={true} />
 
-                        <SelectField name="authorizedGrantTypes" label="Authorised Grant Types"
-                                     helpText="Please select all authorised grant types to allow"
-                                     options={[
-                                         {label: 'Implicit', value: 'IMPLICIT'},
-                                         {label: 'Authorization Code', value: 'AUTHORIZATION_CODE'},
-                                         {label: 'Client Credentials', value: 'CLIENT_CREDENTIALS'},
-                                         {label: 'Password', value: 'PASSWORD'},
-                                         {label: 'Refresh Token', value: 'REFRESH_TOKEN'}
-                                     ]} multiple={true} />
+                <SelectField name="authorizedGrantTypes" label="Authorised Grant Types"
+                             helpText="Please select all authorised grant types to allow"
+                             options={[
+                                 {label: 'Implicit', value: 'IMPLICIT'},
+                                 {label: 'Authorization Code', value: 'AUTHORIZATION_CODE'},
+                                 {label: 'Client Credentials', value: 'CLIENT_CREDENTIALS'},
+                                 {label: 'Password', value: 'PASSWORD'},
+                                 {label: 'Refresh Token', value: 'REFRESH_TOKEN'}
+                             ]} multiple={true} />
 
-                        <SelectField name="scope" label="Scope"
-                                     helpText="Please select all scopes that apply"
-                                     options={[
-                                         {label: 'Read', value: 'READ'},
-                                         {label: 'Write', value: 'WRITE'},
-                                         {label: 'Trust', value: 'TRUST'}
-                                     ]} multiple={true} />
+                <SelectField name="scope" label="Scope"
+                             helpText="Please select all scopes that apply"
+                             options={[
+                                 {label: 'Read', value: 'READ'},
+                                 {label: 'Write', value: 'WRITE'},
+                                 {label: 'Trust', value: 'TRUST'}
+                             ]} multiple={true} />
 
-                        <MultiValuedTextField name="roles" label="Roles"
-                                              placeholder="Enter role name"
-                                              helpText="Please select all roles that apply" />
+                <MultiValuedTextField name="roles" label="Roles"
+                                      placeholder="Enter role name"
+                                      helpText="Please select all roles that apply" />
 
-                        <MultiValuedTextField name="registeredRedirectUris" label="Registered Redirect URLs"
-                                              placeholder="Enter URL to redirect to"
-                                              helpText="Please specify allowed redirect URLs" />
+                <MultiValuedTextField name="registeredRedirectUris" label="Registered Redirect URLs"
+                                      placeholder="Enter URL to redirect to"
+                                      helpText="Please specify allowed redirect URLs" />
 
-                        <button className="btn btn-primary fa-pull-right" type="submit">Save</button>
-                    </form>
-                )}
-            </Form>
-        );
+                <button className="btn btn-primary fa-pull-right" type="submit">Save</button>
+            </form>
+        )
+    }
+
+    render() {
+        return <Form getApi={this.setApi} onChange={this.onChange}>{this.renderForm}</Form>
     }
 }
 
